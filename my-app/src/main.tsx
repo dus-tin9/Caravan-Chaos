3a6c3eac4cbc4acc79a8864b7a2a87205adb76a0
Vexotex <156705937+Vexotex@users.noreply.github.com>
2025-12-13 07:06:25 +0100

    oder Function-calls

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from 'react-dom/client'
 
+function KWtoHP (KW) {
+  return KW * 1.38;
+}
+
 function Car () {
   const brand = "Ford";
   const model = "Mustang";
@@ -11,6 +15,7 @@ function Car () {
       <p>It's a {brand} {model}.</p>
       <p>It has {218 * 1.38} Horspower</p>
       <p>It has {hp} Horspower</p>
+      <p>It has {KWtoHP(218)} Horspower</p>
     </>
   );
 }
