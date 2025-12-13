d3a24192fd22348151aa15eda56785e3bb441b25
Vexotex <156705937+Vexotex@users.noreply.github.com>
2025-12-13 06:49:52 +0100

    Fragement <></> anstatt DIV

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
 
 const myElement = (
-  <div>
+  <>
     <p>I am a paragraph</p>
     <p>I am a paragraph, too</p>
-  </div>
+  </>
 );
 
 createRoot(document.getElementById('root')!).render(
