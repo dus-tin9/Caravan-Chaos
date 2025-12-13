39ff2f210f97e25ffbdc8e8974143a1e64d97e3c
Vexotex <156705937+Vexotex@users.noreply.github.com>
2025-12-13 07:41:47 +0100

    create a simple component

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -2,12 +2,14 @@ import { createRoot } from 'react-dom/client'
 
 
 
-function Fruit () {
-  const x = 5;
-  return(
-    <h1>{(x) < 10 ? "Apple" : "Banana"}</h1>
+function Car () {
+
+  return (
+    <h2>Hi, I am a Car</h2>
   )
 }
+
+
 createRoot(document.getElementById('root')!).render(
-  <Fruit />
+  <Car />
 )
