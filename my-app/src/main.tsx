21864a537f6d2ea5317dd93f3e45604c4baa356e
Vexotex <156705937+Vexotex@users.noreply.github.com>
2025-12-13 07:00:22 +0100

    die Ersten Variablen

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client'
 
 function Car () {
+  const brand = "Ford";
+  const model = "Mustang";
+
   return (
     <>
       <h2>My Car</h2>
-      <p>It's a Ford Mustang.</p>
+      <p>It's a {brand} {model}.</p>
     </>
   );
 }
