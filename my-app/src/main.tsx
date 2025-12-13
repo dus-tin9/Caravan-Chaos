ebbd9f70337bbc2df5a48e5e5cb8220841ad1ff5
Vexotex <156705937+Vexotex@users.noreply.github.com>
2025-12-13 07:04:28 +0100

    in den {} darf JS rein
    aber am besten auserhalb calculieren und dann nur variablen referenzieren

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 function Car () {
   const brand = "Ford";
   const model = "Mustang";
+  const hp = 218 * 1.38;
 
   return (
     <>
       <h2>My Car</h2>
       <p>It's a {brand} {model}.</p>
+      <p>It has {218 * 1.38} Horspower</p>
+      <p>It has {hp} Horspower</p>
     </>
   );
 }
