af6bcbc6f207ca42aca0e3a501aa29cb567a0526
Vexotex <156705937+Vexotex@users.noreply.github.com>
2025-12-13 07:43:19 +0100

    props

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -2,14 +2,14 @@ import { createRoot } from 'react-dom/client'
 
 
 
-function Car () {
+function Car (props) {
 
   return (
-    <h2>Hi, I am a Car</h2>
+    <h2>Hi, I am a {props.color} Car</h2>
   )
 }
 
 
 createRoot(document.getElementById('root')!).render(
-  <Car />
+  <Car color="Red"/>
 )
