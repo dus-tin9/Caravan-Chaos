69e1202ecf49f89f6a23f8527b6b1fc96cd9e9a0
Vexotex <156705937+Vexotex@users.noreply.github.com>
2025-12-13 07:38:29 +0100

    ternary expressions

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -4,12 +4,8 @@ import { createRoot } from 'react-dom/client'
 
 function Fruit () {
   const x = 5;
-  let y = "Apple"
-  if (x > 10){
-    y= "banana";
-  };
   return(
-    <h1>{y}</h1>
+    <h1>{(x) < 10 ? "Apple" : "Banana"}</h1>
   )
 }
 createRoot(document.getElementById('root')!).render(
