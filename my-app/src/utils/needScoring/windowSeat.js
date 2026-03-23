a0bc363a1275aa25a3efc0daaa956c16cd094a37
Vexotex <156705937+Vexotex@users.noreply.github.com>
2026-03-23 20:20:11 +0100

    parserstruktur angelegt

---
+++ my-app/src/utils/needScoring/windowSeat.js
@@ -0,0 +1,5 @@
+export default function calculateWindowSeatScore(person, seat, allSeats) {
+  // Logik: z.B. +10 wenn am Fenster, 0 sonst
+  // seat und allSeats stehen zur Verfügung für Nachbarschafts-Logik
+  return 10;
+}
