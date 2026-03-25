a3793b2c05ae5f847fb89902025020b7a1c5f5ee
BasuraGit <Jan.Mahnke@t-online.de>
2026-03-25 12:04:47 +0100

    Buttons in Level Ansicht hinzugefügt
     Buttons die den Seiten Status in App aktualisieren und eine andere Component aufrufen

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,28 @@
+import { useState } from 'react'
 import Level from './components/Level'
 
 
 export default function App() {
 
+  const [SiteState, setSiteState] = useState("Level")
+
+  function currentSiteState(State){
+    switch(State){
+        case "Level":
+            return( <Level setSiteState={setSiteState}/> )
+        case "MainMenu":
+            return( <MainMenu setSiteState={setSiteState}/> )
+        case "LevelRecap":
+            return( <LevelRecap setSiteState={setSiteState}/> )
+        case "LevelAuswahl":
+            return( <LevelAuswahl setSiteState={setSiteState}/> )
+    }
+  }
+
 
   return (
-    <Level/>
+    <div>
+        {currentSiteState(SiteState)}
+    </div>
   )
 }
